3d5b3ffd997c7552bb513a91cb8464e661db564e
jimmy-kyalo <kyalo90@gmail.com>
2023-01-14 10:51:17 +0300

    Worker class

--- server/src/IMAP.ts
+++ server/src/IMAP.ts
@@ -1,2 +1,44 @@
-export interface IMailbox {}
-export const IMAP = () => {}
+const ImapClient = require('emailjs-imap-client')
+import { ParsedMail, simpleParser } from 'mailparser'
+import { IServerInfo } from './ServerInfo'
+
+export interface ICallOptions {
+  mailbox: string
+  id?: number
+}
+
+export interface IMessage {
+  id: string
+  date: string
+  from: string
+  subject: string
+  body?: string
+}
+
+export interface IMailbox {
+  name: string
+  path: string
+}
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+
+export class Worker {
+  private static serverInfo: IServerInfo
+  constructor(inServerInfo: IServerInfo) {
+    Worker.serverInfo = inServerInfo
+  }
+
+  private async connectToServer(): Promise<any> {
+    const client: any = new ImapClient.default(
+      Worker.serverInfo.imap.host,
+      Worker.serverInfo.imap.port,
+      { auth: Worker.serverInfo.imap.auth }
+    )
+    client.logLevel = client.LOG_LEVEL_NONE
+    client.onerror = (inError: Error) => {
+      console.log('IMAP.Worker.listMailboxes(): Connection error', inError)
+    }
+    await client.connect()
+    return client
+  }
+}
--- server/src/SMTP.ts
+++ server/src/SMTP.ts
@@ -1,1 +1,29 @@
-export const SMTP = () => {}
+import Mail from 'nodemailer/lib/mailer'
+import * as nodemailer from 'nodemailer'
+import { SendMailOptions, SentMessageInfo } from 'nodemailer'
+import { IServerInfo } from './ServerInfo'
+
+// const nodemailer = require('nodemailer')
+
+export class Worker {
+  private static serverInfo: IServerInfo
+  constructor(inServerInfo: IServerInfo) {
+    Worker.serverInfo = inServerInfo
+  }
+
+  public sendMessage(inOptions: SendMailOptions): Promise<void> {
+    return new Promise((inResolve, inReject): void => {
+      const transport: Mail = nodemailer.createTransport(Worker.serverInfo.smtp)
+      transport.sendMail(
+        inOptions,
+        (inError: Error | null, inInfo: SendMailOptions) => {
+          if (inError) {
+            inReject(inError)
+          } else {
+            inResolve()
+          }
+        }
+      )
+    })
+  }
+}
